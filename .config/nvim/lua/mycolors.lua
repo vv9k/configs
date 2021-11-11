5bdc4037ff49c59c3d26c589f8abb3b34dbabc78
Wojciech Kępka <wojciech@wkepka.dev>
2021-11-11 17:50:14 +0100

    Cleanup configs

--- .config/nvim/lua/mycolors.lua
+++ .config/nvim/lua/mycolors.lua
@@ -3,7 +3,6 @@ if vim.fn.has("termguicolors") then
 end
 
 vim.o.background = 'dark'
---vim.o.background = 'light'
 
 vim.g.ayucolor = 'mirage'
 vim.g.gh_color = 'soft'
@@ -11,10 +10,5 @@ vim.g.gruvbox_contrast_dark = 'medium'
 vim.g.gruvbox_contrast_light = 'hard'
 vim.g.airline_powerline_fonts = 1
 
---vim.cmd('colorscheme nord')
 vim.cmd('colorscheme bogster')
---vim.cmd('colorscheme ghdark')
---vim.cmd('colorscheme ayu')
---vim.cmd('colorscheme gruvbox')
---vim.cmd('colorscheme solarized8')
 --vim.cmd('colorscheme onedark')
