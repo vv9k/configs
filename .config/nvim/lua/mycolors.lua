c24e9092d505766b8a54e3ca07c1539ecc3d72c7
Wojciech Kępka <wojciech@wkepka.dev>
2022-06-12 21:21:27 +0200

    Update config and mold templates

--- .config/nvim/lua/mycolors.lua
+++ .config/nvim/lua/mycolors.lua
@@ -2,8 +2,7 @@ if vim.fn.has("termguicolors") then
   vim.o.termguicolors = true
 end
 
---vim.o.background = 'light'
---vim.o.background = 'dark'
+vim.o.background = 'dark'
 
 vim.g.ayucolor = 'mirage'
 vim.g.gh_color = 'soft'
@@ -11,8 +10,5 @@ vim.g.gruvbox_contrast_dark = 'medium'
 vim.g.gruvbox_contrast_light = 'hard'
 vim.g.airline_powerline_fonts = 1
 
---vim.cmd('colorscheme bogster')
+vim.cmd('colorscheme bogster')
 --vim.cmd('colorscheme onedark')
---require('github-theme').setup()
-vim.cmd('colorscheme nightfox')
---require('nightfox').load('dayfox')
